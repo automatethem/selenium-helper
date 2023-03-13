e6a8792eaeefde8be4b6996cf8610f9682631866
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-13 11:15:29 +0900

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -1,12 +1,7 @@
-import pynecone as pc
 import selenium_supporter
-import time
 
 debugger_address = "127.0.0.1:1001"
 chrome_driver = selenium_supporter.drivers.ChromeDebuggingDriver(debugger_address)
 driver = chrome_driver.get_driver()
 
-url = driver.current_url
-print(url)
-
-driver.get("https://www.jobkorea.co.kr/Corp/Person/PositionOfferList?offerIdx=9745296")
+driver.get("https://www.automatethem.com")

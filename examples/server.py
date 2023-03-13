c19acaa17918d6b8508c9269e298c38f2e08727e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-13 11:14:50 +0900

    Update server.py

--- examples/server.py
+++ examples/server.py
@@ -5,4 +5,4 @@ remote_debugging_address = debugger_address.split(':')[0]
 remote_debugging_port = int(debugger_address.split(':')[1])
 #print(remote_debugging_address) #127.0.0.1
 #print(remote_debugging_port) #1001
-selenium_supporter.utils.open_chrome_web_browser_with_remote_debugging_mode(remote_debugging_port, remote_debugging_address, user_data_dir=None, proxy_server=None, headless=False)
+selenium_supporter.utils.open_chrome_web_browser_with_remote_debugging_mode(remote_debugging_port, remote_debugging_address)

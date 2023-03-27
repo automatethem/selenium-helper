bf9a22f98b1f400f0adde68b865fa8793f56cdeb
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-27 13:22:17 +0900

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -4,4 +4,4 @@ debugger_address = "127.0.0.1:1001"
 chrome_driver = selenium_supporter.drivers.ChromeDebuggingDriver(debugger_address)
 driver = chrome_driver.get_driver()
 
-driver.get("https://www.automatethem.com")
+driver.get("http://www.automatethem.com")

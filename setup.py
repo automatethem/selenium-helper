4d87804f15ef537d8f1245e4352f24838f40ca1e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-08 20:22:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='selenium-supporter',
-	version='0.0.15',
+	version='0.0.16',
 	description='Selenium supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
